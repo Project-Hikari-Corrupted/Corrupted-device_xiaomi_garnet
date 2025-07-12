5f68b21552c69e13e72ef2c05b04960679652649
Hikari-Soni <247soni01@gmail.com>
2025-07-12 09:20:33 +0200

    Superior

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+WITH_GMS := true
+# TARGET_BUILD_VARIANT := user
 
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
